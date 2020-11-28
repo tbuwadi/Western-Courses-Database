2a547ea189643babe70e201289a26f449f68f60e
Tala Buwadi <tbuwadi@uwo.ca>
2020-11-27 22:18:57 -0500

    changed path

--- a3pen/index.php
+++ a3pen/index.php
@@ -3,7 +3,7 @@
 <head>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="mystyle.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php
